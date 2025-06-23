2f3209182b06c357a2101a1155ef6ab739c1c642
kushaljk11 <np05cp4a240142@iic.edu.np>
2025-06-23 11:28:49 +0545

    .

---
+++ mern.js
@@ -0,0 +1,40 @@
+// function mern() {
+//     var a = 10
+//     var b = 20
+//     let a = 23
+//     var c = a + b
+//     console.log(c)
+// }
+// mern()
+
+//arrow function
+// const arrow = () => {
+//     console.log("Hello, the sum of 1 2 is");
+//     let a = 1;
+//     let b = 2;
+//     let c = a + b;
+//     console.log(c);
+//     return c;
+//  }
+// arrow();
+
+// function add() {
+//     console.log(1 + 2);
+// }
+// add();
+
+function add() {
+    let a = 20
+    let b = 30
+    let c = a + b
+    console.log(c)
+}
+add();
+
+const arrow = () => {
+    let a = 20
+    let b = 20
+    let c = a + b 
+    console.log("The sum of a and b is: " + c);
+}
+arrow()
