9216872b6e63a050d5872a03a927fe790c152f77
kushaljk11 <np05cp4a240142@iic.edu.np>
2025-06-24 09:56:00 +0545

    second commit

--- mern.js
+++ mern.js
@@ -24,18 +24,59 @@
 // add();
 
 
-function add() {
-    let a = 20
-    let b = 30
-    let c = a + b
-    console.log(c)
-}
-add();
+// function add() {
+//     let a = 20
+//     let b = 30
+//     let c = a + b
+//     console.log(c)
+// }
+// add();
 
-const arrow = () => {
-    let a = 20
-    let b = 20
-    let c = a + b 
-    console.log("The sum of a and b is: " + c);
+// const arrow = () => {
+//     let a = 20
+//     let b = 20
+//     let c = a + b 
+//     console.log("The sum of a and b is: " + c);
+// }
+// arrow()
+
+//objects -> key value pair
+// let person = {
+//     name : "mama" //where person is a object abd name is a key and mama is a value
+    
+// }
+
+let student = {
+    name: "Mama",
+    class: "1",
+    marks: {
+        math: 95,
+        science: 90,
+        english: 85
+    }
 }
-arrow()
+
+for (let marks in student.marks) {
+    console.log(`Subject: ${student.marks[marks].subject}, Score: ${student.marks[marks].score}`);
+}
+
+
+let totalscore = 0;
+let studentnumber = 0
+
+for (let marks in student.marks) {
+    totalscore = student.marks[marks] + totalscore;
+    studentnumber++;
+}
+console.log("Total score: " + totalscore);
+
+let average = totalscore / studentnumber;
+console.log("Average score: " + average);
+
+if (average >= 40) {
+    console.log("You are passed")
+} else {
+    console.log("Fail")
+}
+
+
