664ff8148019587a0b7c5521b55525ea3da2defc
kushaljk11 <np05cp4a240142@iic.edu.np>
2025-06-24 11:05:25 +0545

    second commit

--- day2.js
+++ day2.js
@@ -18,35 +18,104 @@
 // console.log(person["address"]);
 
 
+// let student = {
+//     name: "Mama",
+//     class: "1",
+//     marks: {
+//         math: 95,
+//         science: 90,
+//         english: 85
+//     }
+// }
+
+// for (let marks in student.marks) {
+//     console.log(`Subject: ${student.marks[marks].subject}, Score: ${student.marks[marks].score}`);
+// }
+
+
+// let totalscore = 0;
+// let studentnumber = 0
+
+// for (let marks in student.marks) {
+//     totalscore = student.marks[marks] + totalscore;
+//     studentnumber++;
+// }
+// console.log("Total score: " + totalscore);
+
+// let average = totalscore / studentnumber;
+// console.log("Average score: " + average);
+
+// if (average >= 40) {
+//     console.log("You are passed")
+// } else {
+//     console.log("Fail")
+// }
+
 let student = {
-    name: "Mama",
-    class: "1",
-    marks: {
-        math: 95,
-        science: 90,
-        english: 85
+    student1: {
+        name: "Mama",
+        class: "1",
+        marks: {
+            math: 95,
+            science: 90,
+            english: 85
+        }
+    },    
+    student2: {
+        name: "Mama2",
+        class: "1",
+        marks: {
+            math: 95,
+            science: 90,
+            english: 85
+        }
+    },
+    student3: {
+        name: "Mama3",
+        class: "1",
+        marks: {
+            math: 95,
+            science: 90,
+            english: 85
+        }
+    }
+}
+for (let stud in student) {
+    console.log(stud, student[stud]);
+}
+
+for (let stud in student) {
+    console.log(stud, student[stud].marks);
+}
+let totalscore = 0;
+let studentnumber = 0;
+for (let stud in student) {
+    for (let marks in student[stud].marks) {
+        totalscore = student[stud].marks[marks] + totalscore;
+        studentnumber++;
+    }
+    console.log("Student name: " + student[stud].name);
+    console.log("Total score: " + totalscore);
+
+    let average = totalscore / studentnumber;
+    console.log("Average score: " + average);
+
+    if (average >= 40) {
+    console.log("You are passed");
+    } else {
+        console.log("Fail");
     }
 }
 
-for (let marks in student.marks) {
-    console.log(`Subject: ${student.marks[marks].subject}, Score: ${student.marks[marks].score}`);
+let classtotalmarks = 0;
+let classstudentnumber = 0;
+for (let stud in student) {
+    for (let marks in student[stud].marks) {
+        classtotalmarks = student[stud].marks[marks] + classtotalmarks;
+        classstudentnumber++;
+    }
 }
 
-
-let totalscore = 0;
-let studentnumber = 0
-
-for (let marks in student.marks) {
-    totalscore = student.marks[marks] + totalscore;
-    studentnumber++;
-}
-console.log("Total score: " + totalscore);
-
-let average = totalscore / studentnumber;
-console.log("Average score: " + average);
-
-if (average >= 40) {
-    console.log("You are passed")
-} else {
-    console.log("Fail")
-}
+let classaverage = classtotalmarks / classstudentnumber;
+console.log("Class average score: " + classaverage);
+console.log("Class total marks: " + classtotalmarks);
